a91ba2d752c4ddd2da6737f8a089afbb4efeb110
Dani Latorre <danilatorre@genially.com>
2023-04-19 17:18:19 +0200

    PoC to use testcontainers with mongo client

---
+++ __tests__/mongo-client.test.js
@@ -0,0 +1,71 @@
+let mongoContainer;
+var mongoClient;
+
+describe('mongo client with test containers', () => {
+  jest.setTimeout(240_000);
+  beforeAll(async () => {
+
+    mongoContainer = new GenericContainer('mongo:4.4.20')
+      .withExposedPorts(DB_EXPOSED_PORT)
+      .withEnvironment({ MONGO_INITDB_DATABASE: DB_NAME })
+      .withWaitStrategy(Wait.forLogMessage(/.*waiting for connections.*/i))
+      .withStartupTimeout(5_000);
+    await mongoContainer.start().catch(err => console.error(err));
+    (await mongoContainer.logs())
+      .on("data", line => console.log(line))
+      .on("err", line => console.error(line))
+      .on("end", () => console.log("Stream closed"));
+
+    //const mongoUri = `mongodb://localhost:27017/${DB_NAME}`
+    const mongoUri = getUriForMongoContainer(mongoContainer);
+    console.log(mongoUri)
+
+    await connectToSessionMongoDb(mongoUri);
+
+  });
+
+  afterAll(async () => {
+    await disconnectToSessionMongoDb();
+    await mongoContainer.stop();
+  });
+
+  it('it works!', async () => {
+    const userId = 'irrelevant id';
+    await mongoClient.db().collection('users').insertOne({ id: userId });
+
+    const document = await mongoClient.db().collection('users').findOne();
+
+    expect(document.id).toBe(userId);
+  });
+});
+
+const { MongoClient } = require('mongodb');
+const { GenericContainer, Wait, MongoDBContainer } = require('testcontainers');
+
+
+const DB_NAME = 'test_db';
+const DB_EXPOSED_PORT = 27017;
+
+const getUriForMongoContainer = (mongoContainer) => {
+  return `mongodb://${mongoContainer.getHost()}:${mongoContainer.getMappedPort(
+    DB_EXPOSED_PORT,
+  )}/${DB_NAME}`;
+};
+
+const connectToSessionMongoDb = async (mongoUri) => {
+  mongoClient = new MongoClient(mongoUri);
+  try {
+    console.log('connecting')
+    await mongoClient.connect();
+    console.log('connected')
+  } catch (error) {
+    console.error(error)
+  }
+};
+
+
+const disconnectToSessionMongoDb = async () => {
+  if (mongoClient) {
+    await mongoClient.close();
+  }
+};
