3ffe55de417c8894b52e5dfb485829d51ee286b8
Dani Latorre <dani@danilat.com>
2023-04-20 16:44:13 +0200

    Adding simultanous usage for mongoose and official mongodb libraries

--- __tests__/mongo-client.test.js
+++ __tests__/mongo-client.test.js
@@ -1,43 +1,48 @@
+const { MongoClient } = require('mongodb');
+const { GenericContainer, Wait, MongoDBContainer } = require('testcontainers');
+const mongoose = require('mongoose');
+
+
 let mongoContainer;
-var mongoClient;
+let mongoClient;
 
 describe('mongo client with test containers', () => {
-  jest.setTimeout(240_000);
+  jest.setTimeout(30_000);
   beforeAll(async () => {
 
     mongoContainer = new GenericContainer('mongo:4.4.20')
       .withExposedPorts(DB_EXPOSED_PORT)
       .withEnvironment({ MONGO_INITDB_DATABASE: DB_NAME })
-      .withWaitStrategy(Wait.forLogMessage(/.*waiting for connections.*/i))
-      .withStartupTimeout(5_000);
+      .withWaitStrategy(Wait.forLogMessage(/.*waiting for connections.*/i));
     mongoContainer = await mongoContainer.start().catch(err => console.error(err));
     
-    //const mongoUri = `mongodb://localhost:27017/${DB_NAME}`
     const mongoUri = getUriForMongoContainer(mongoContainer);
     console.log(mongoUri)
 
-    await connectToSessionMongoDb(mongoUri);
-
+    mongoClient = await connectToSessionMongoDb(mongoUri);
+    await mongoose.connect(mongoUri);
   });
 
   afterAll(async () => {
-    await disconnectToSessionMongoDb();
+    await mongoClient.close();
+    await mongoose.disconnect();
     await mongoContainer.stop();
   });
 
   it('it works!', async () => {
     const userId = 'irrelevant id';
     await mongoClient.db().collection('users').insertOne({ id: userId });
+    const Cat = mongoose.model('Cat', { name: String });
+    const kitty = new Cat({ name: 'Zildjian' });
+    await kitty.save();
 
-    const document = await mongoClient.db().collection('users').findOne();
+    const user = await mongoClient.db().collection('users').findOne();
+    expect(user.id).toBe(userId);
 
-    expect(document.id).toBe(userId);
+    const cat = await Cat.findOne({ name: kitty.name })
+    expect(cat.name).toBe(kitty.name);
   });
 });
-
-const { MongoClient } = require('mongodb');
-const { GenericContainer, Wait, MongoDBContainer } = require('testcontainers');
-
 
 const DB_NAME = 'test_db';
 const DB_EXPOSED_PORT = 27017;
@@ -49,15 +54,12 @@ const getUriForMongoContainer = (mongoContainer) => {
 };
 
 const connectToSessionMongoDb = async (mongoUri) => {
-  mongoClient = new MongoClient(mongoUri);
+  const mongoClient = new MongoClient(mongoUri);
   try {
     await mongoClient.connect();
+    return mongoClient;
   } catch (error) {
     console.error(error)
+    throw error;
   }
-};
-
-
-const disconnectToSessionMongoDb = async () => {
-  await mongoClient.close();
 };
